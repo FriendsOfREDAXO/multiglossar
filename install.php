beba9f39e3aea33e573437114be5c698942270ac
Thomas Skerbis <thomas.skerbis@mac.com>
2017-08-15 13:55:44 +0200

    textfield_css bei Installation festlegen

--- install.php
+++ install.php
@@ -19,7 +19,26 @@ CREATE TABLE IF NOT EXISTS `rex_multiglossar` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 ');
 
+
+//  MarkItUp
+if(rex_addon::get('markitup')->isAvailable()) {
+  if (!markitup::profileExists('multiglossar')) {
+  markitup::insertProfile ('multiglossar', $this->i18n('glossar_markitupinfo'), 'textile', 300, 800, 'relative', 'bold,italic,underline,deleted,quote,sub,sup,code,unorderedlist,grouplink[internal|external|mailto]');
+  }
+  $editor = 'markitupEditor-multiglossar';
+}
+
+//  Redactor2
+if (rex_addon::get('redactor2')->isAvailable()) {
+  if (!redactor2::profileExists('multiglossar')) {
+    redactor2::insertProfile('multiglossar', $this->i18n('glossar_redactorinfo'), '300', '800', 'relative','bold, italic, underline, deleted, sub, sup,  unorderedlist, orderedlist, grouplink[email|external|internal|media], cleaner');
+  }
+  $editor = 'redactorEditor2-multiglossar';
+}
+
+
 if (!$this->hasConfig()) {
-  $this->setConfig('status','deaktiviert');
+    $this->setConfig('status','deaktiviert');
+    $this->setConfig('textfield_css','$editor');  
 }
 
