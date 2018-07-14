fc088ace88b4081add4ac7d19959550726386f1a
Thomas Skerbis <thomas.skerbis@mac.com>
2018-07-14 23:56:29 +0200

    table setup

--- update.php
+++ update.php
@@ -1,4 +1,5 @@
 <?php
+// glossary table
 rex_sql_table::get(rex::getTable('multiglossar'))
     ->ensureColumn(new rex_sql_column('pid', 'int(10) unsigned', false, null, 'auto_increment'))
     ->ensureColumn(new rex_sql_column('id', 'int(10) unsigned'))
@@ -14,4 +15,12 @@ rex_sql_table::get(rex::getTable('multiglossar'))
     ->ensureColumn(new rex_sql_column('updatedate', 'datetime'))
     ->ensureColumn(new rex_sql_column('revision', 'int(10) unsigned'))
     ->setPrimaryKey('pid')
+    ->ensure();
+// Cache
+rex_sql_table::get(rex::getTable('multiglossar_cache'))
+    ->ensureColumn(new rex_sql_column('article_id', 'int(11)'))
+    ->ensureColumn(new rex_sql_column('clang_id', 'int(11)'))
+    ->ensureColumn(new rex_sql_column('content', 'text'))
+    ->ensureColumn(new rex_sql_column('url', 'text'))
+    ->ensureColumn(new rex_sql_column('query_string', 'text'))
     ->ensure();
